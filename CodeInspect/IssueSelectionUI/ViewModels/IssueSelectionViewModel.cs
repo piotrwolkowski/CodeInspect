38ae42d2aaa339aebec60e876d5f6fac40eb57d4
piotrwolkowski <piotr.wolkowski@gmail.com>
2014-04-13 23:08:50 +0100

    event communication between modules
    IssueListModule communicates with IssueSelectionModule

--- CodeInspect/IssueSelectionUI/ViewModels/IssueSelectionViewModel.cs
+++ CodeInspect/IssueSelectionUI/ViewModels/IssueSelectionViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CodeInspectEntities;
+using CodeInspectEntities.CompositeEvents;
+using Microsoft.Practices.Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace IssueSelectionUI.ViewModels
 {
-    public class IssueSelectionViewModel
+    public class IssueSelectionViewModel : NotifyPropertyChangedImplementation
     {
+        private IssueWithDescription selectedIssue;
+        public IssueWithDescription SelectedIssue
+        {
+            get
+            {
+                return this.selectedIssue;
+            }
+            set
+            {
+                this.selectedIssue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public IssueSelectionViewModel(IEventAggregator eventAggregator)
+        {
+            SelectedIssueChangedEvent selectedIssueChangedEvent = eventAggregator.GetEvent<SelectedIssueChangedEvent>();
+            selectedIssueChangedEvent.Subscribe(OnSelectedIssueChanged);
+        }
+
+        private void OnSelectedIssueChanged(IssueWithDescription selectedIssue)
+        {
+            this.SelectedIssue = selectedIssue;
+        }
     }
 }
